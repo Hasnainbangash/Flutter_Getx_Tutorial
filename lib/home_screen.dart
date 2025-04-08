048100c433fd075eb432487d5a5d187748e35947
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-08 22:07:57 +0500

    SOme issues get resolved

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GetX Tutorials'),
+        title: Text('GetX Tutorial'),
       ),
       body: Column(
         children: [
