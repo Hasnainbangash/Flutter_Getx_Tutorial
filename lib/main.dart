988b0075459218f0b894c2f20f19adad86f295c6
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-11 20:17:53 +0500

    ROutes are created for every page to move to the next screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_tutorial/home_screen.dart';
 import 'package:flutter_getx_tutorial/Navigation and Routes/getx_navigation_and_routes.dart';
+import 'package:flutter_getx_tutorial/Navigation and Routes/screen_one.dart';
+import 'package:flutter_getx_tutorial/Navigation and Routes/screen_Two.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -20,6 +22,12 @@ class MyApp extends StatelessWidget {
       ),
       // home: const HomeScreen(),
       home: const NavigationAndRoutes(),
+      // Added the routes to to the screens
+      getPages: [
+        GetPage(name: '/', page: () => NavigationAndRoutes()),
+        GetPage(name: '/screenOne', page: () => ScreenOne()),
+        GetPage(name: '/screenTwo', page: () => ScreenTwo()),
+      ],
     );
   }
 }
