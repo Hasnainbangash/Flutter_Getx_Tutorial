cb1bf06b6a32a7babbb95bcc1f6e03e89020aa1b
Bangash38 <bangashmhasnain38@gmail.com>
2025-04-12 19:14:41 +0500

    Wrap the container inside the column and added the another container chage its color and height and work done of getx height and width

--- lib/getx_height_and_width.dart
+++ lib/getx_height_and_width.dart
@@ -19,23 +19,45 @@ class _GetXHeightAndWidthState extends State<GetXHeightAndWidth> {
       appBar: AppBar(
         title: Text('GetX Tutorials'),
       ),
-      body: Container(
-        // Simple way to give the height
-        // height: 100,
+      body: Column(
+        children: [
+          Container(
+            // Simple way to give the height
+            // height: 100,
 
-        // Using the media query to get height
-        // height: MediaQuery.of(context).size.height * 0.3,
+            // Using the media query to get height
+            // height: MediaQuery.of(context).size.height * 0.3,
 
-        // Using the classic way to give height
-        // height: height * .2,
+            // Using the classic way to give height
+            // height: height * .2,
 
-        // Using the getx to give height and width
-        height: Get.height * 0.2,
-        width: Get.width * .8,
-        color: Colors.red,
-        child: Center(
-          child: Text('Center'),
-        ),
+            // Using the getx to give height and width
+            height: Get.height * 0.2,
+            width: Get.width * .8,
+            color: Colors.red,
+            child: Center(
+              child: Text('Center'),
+            ),
+          ),
+          Container(
+            // Simple way to give the height
+            // height: 100,
+
+            // Using the media query to get height
+            // height: MediaQuery.of(context).size.height * 0.3,
+
+            // Using the classic way to give height
+            // height: height * .2,
+
+            // Using the getx to give height and width
+            height: Get.height * 0.1,
+            width: Get.width * .8,
+            color: Colors.green,
+            child: Center(
+              child: Text('Center'),
+            ),
+          ),
+        ],
       ),
     );
   }
